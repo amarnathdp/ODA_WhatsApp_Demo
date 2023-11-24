5f9e2572bc4f8c1c188b9ced87fb15a0309f6ff6
amarnathdp <118151760+amarnathdp@users.noreply.github.com>
2023-11-24 11:32:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const webhook = new WebhookClient({
 
 webhook
     .on(WebhookEvent.ERROR, err => console.log('Ansh webhook Error:', err.message))
-    .on(WebhookEvent.MESSAGE_SENT, message => console.log('Ansh Message to chatbot:', message));
+    .on(WebhookEvent.MESSAGE_SENT, message => console.log('End User Message to chatbot:', message));
 app.post('/bot/message', webhook.receiver()); // receive bot messages
 
 
